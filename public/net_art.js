1b45dfc4541917501bd325daba96b4e91408185f
Nathan Phan <nathan.phan2320@gmail.com>
2023-05-10 13:46:45 +1000

    net art finalised with comments

--- public/net_art.js
+++ public/net_art.js
@@ -11,40 +11,64 @@ cnv.height = innerHeight
 resizeCanvas ()
 window.onresize = resizeCanvas
 
+// Changes the canvas size to the same size as the page
 function resizeCanvas () {
     cnv.width = innerWidth
     cnv.height = innerHeight
 }
 
+// array to store the coordinates
 const coordinates = [];
 
+// assign the onclick assignment to the argument 'e'
 function add_coordinate (e) {
+    
+    // pushes offsetX and offsetY coordinates as 'x' and 'y' into the 'coordinates' array
     coordinates.push ({x : e.offsetX, y: e.offsetY})
 }
 
+// calls function 'add_coordinate' everytime mouse is clicked
 cnv.onclick = add_coordinate
 
+
+// get context objective for the canvas 
 const ctx = cnv.getContext ('2d')
 
-draw_frame ()
+// calls the function 'draw_frame'
+requestAnimationFrame (draw_frame)
 
 function draw_frame () {
+
+    // set the background colour
     ctx.fillStyle = "#2B2D42"
     ctx.fillRect (0, 0, cnv.width, cnv.height)
 
-    ctx.fillStyle = "#EF233C"
+    // changes all the opacity to 0.8
+    ctx.globalAlpha = 0.8
 
+    // goes through each coordinate in the array
     coordinates.forEach (p => {
-        let x_pos = p.x - 25;
-        let y_pos = p.y - 25;
-        ctx.fillRect (x_pos, y_pos, 50, 50);
+
+        // random values for rgb colours
+        var r = 0 + 255 * Math.random();         
+        var b = 0 + 255 * Math.random();
+        var g = 0 + 255 * Math.random();
+
+        // random values for rectangle width and height
+        let w = 300 * Math.random ();
+        let h = 300 * Math.random ();
+
+        // the center of the squares created is positioned where the mouse is
+        let x_pos = p.x - w / 2;
+        let y_pos = p.y - h / 2;
+
+        // fill the squares with the random rgb colour created
+        ctx.fillStyle = "rgb(" + r + ", " + g + ", " + b + ")";     
+
+        // creates the square with the random width and height value created
+        ctx.fillRect (x_pos, y_pos, w, h);
     })
 
-    requestAnimationFrame (draw_frame)
+    // allowes the squares to change every second
+    setTimeout(draw_frame, 1000)
 }
-
-function move_squares () {
-    coordinates.forEach((val, index) => coordinates[index] = val += 1);
-}
-
-requestAnimationFrame (draw_frame)
